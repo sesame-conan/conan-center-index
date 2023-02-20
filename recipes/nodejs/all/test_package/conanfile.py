9a172a80ac6b34ffedf35b95700f99b988acb530
Orhun Birsoy <orhunbirsoy@gmail.com>
2023-02-20 08:05:46 -0700

    Fix nodejs for conan 2.0
    Add Linux profile
    
    Add Windows profile

--- recipes/nodejs/all/test_package/conanfile.py
+++ recipes/nodejs/all/test_package/conanfile.py
@@ -5,8 +5,11 @@ from conan.tools.build import can_run
 class TestPackageConan(ConanFile):
 
     settings = "os", "arch"
+
     test_type = "explicit"
 
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
     def build_requirements(self):
         self.tool_requires(self.tested_reference_str)
 
